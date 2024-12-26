296addff57c1125e91dd68da9ec5727dc7557bce
PrabhatTheCoder <117260172+PrabhatTheCoder@users.noreply.github.com>
2024-12-27 03:38:36 +0530

    Time: 17 ms (6.16%), Space: 13.3 MB (7.53%) - LeetHub

--- 0088-merge-sorted-array/0088-merge-sorted-array.py
+++ 0088-merge-sorted-array/0088-merge-sorted-array.py
@@ -7,25 +7,25 @@ class Solution(object):
         :type n: int
         :rtype: None Do not return anything, modify nums1 in-place instead.
         """
-        ans = []
-        i = 0 
-        j = 0 
-
-        while i <= m - 1 and j <= n - 1:
-            if nums1[i] <= nums2[j]:
-                ans.append(nums1[i])
-                i += 1
+        
+        
+        k = m + n -1
+        m = m-1
+        n = n-1
+        
+        while  0 <= m and 0 <= n:
+            if nums1[m] > nums2[n]:
+                nums1[k] = nums1[m]
+                m -= 1
             else:
-                ans.append(nums2[j])
-                j += 1
+                nums1[k] = nums2[n]
+                n -= 1
+            k -= 1
 
-        while i <= m - 1:
-            ans.append(nums1[i])
-            i += 1
+        while n > m:
+            nums1[k] = nums2[n]
+            n -= 1
+            k -= 1
 
-        while j <= n - 1:
-            ans.append(nums2[j])
-            j += 1
 
-        for k in range(m + n):
-            nums1[k] = ans[k]
+        return nums1
