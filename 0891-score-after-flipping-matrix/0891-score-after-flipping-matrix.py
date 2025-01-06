42d38e5ce46c1d2ff37711982d87be85233c81e1
PrabhatTheCoder <117260172+PrabhatTheCoder@users.noreply.github.com>
2025-01-06 15:49:33 +0530

    Time: 0 ms (100%), Space: 12.3 MB (25.93%) - LeetHub

---
+++ 0891-score-after-flipping-matrix/0891-score-after-flipping-matrix.py
@@ -0,0 +1,45 @@
+class Solution(object):
+    def matrixScore(self, grid):
+        """
+        :type grid: List[List[int]]
+        :rtype: int
+        """
+
+        # making 1st column all 1's
+        for i in range(len(grid)):
+            if grid[i][0] == 0:
+                for j in range(len(grid[0])):
+                    if grid[i][j] == 0:
+                        grid[i][j] = 1
+                    else:
+                        grid[i][j] = 0
+
+
+        # find no of zeros and one's if zeros > ones then flip the column
+        for j in range(len(grid[0])):
+            zeros = 0
+            ones = 0
+
+            for i in range(len(grid)):
+                if grid[i][j] == 0:
+                    zeros += 1
+                else:
+                    ones += 1
+            
+            if zeros > ones:
+                for i in range(len(grid)):
+                    if grid[i][j] == 0:
+                        grid[i][j] = 1
+                    else:
+                        grid[i][j] = 0
+
+        # Calculating Matrix Score   8 4 2 1
+        score = 0
+        for i in range(len(grid)):
+            x = 1
+            for j in range(len(grid[0])-1,-1,-1):
+                score += grid[i][j]*x
+                x *= 2
+        return score
+
+        
