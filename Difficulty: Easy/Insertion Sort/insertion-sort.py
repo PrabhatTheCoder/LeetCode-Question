b8402b023143e4517763b4a11757bb3b8f84c7bd
PrabhatTheCoder <117260172+PrabhatTheCoder@users.noreply.github.com>
2025-01-15 12:25:19 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Easy/Insertion Sort/insertion-sort.py
@@ -0,0 +1,33 @@
+#{ 
+ # Driver Code Starts
+#Initial Template for Python 3
+
+# } Driver Code Ends
+
+# Please change the array in-place
+class Solution:
+    def insertionSort(self, arr):
+        #code here
+        for i in range(1,len(arr)):
+    
+            while arr[i] < arr[i-1] and i > 0:
+                arr[i], arr[i-1] = arr[i-1], arr[i]
+                i -= 1
+        return arr
+
+#{ 
+ # Driver Code Starts.
+#Initial Template for Python 3
+
+if __name__ == "__main__":
+    t = int(input())
+    while t > 0:
+        arr = list(map(int, input().split()))
+        ob = Solution()
+        ob.insertionSort(arr)
+        print(*arr)
+        print("~")
+        t -= 1
+
+
+# } Driver Code Ends
