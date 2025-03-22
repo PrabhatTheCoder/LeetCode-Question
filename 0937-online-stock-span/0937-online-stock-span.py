3dca763722f6064e424c01f7e71feda96a5d27f7
PrabhatTheCoder <117260172+PrabhatTheCoder@users.noreply.github.com>
2025-03-22 17:57:44 +0530

    Time: 222 ms (5.01%), Space: 22.2 MB (90.68%) - LeetHub

--- 0937-online-stock-span/0937-online-stock-span.py
+++ 0937-online-stock-span/0937-online-stock-span.py
@@ -2,15 +2,18 @@ class StockSpanner:
 
     def __init__(self):
         self.stack = []
-        self.prices = []
 
     def next(self, price: int) -> int:
         span = 1
-        while self.prices and self.prices[-1] <= price:
-            span += self.stack.pop()
-            self.prices.pop()
-        self.stack.append(span)
-        self.prices.append(price)
+        
+        # While the stack is not empty and the current price is greater or equal to the top of the stack
+        while self.stack and price >= self.stack[-1][0]:
+            span += self.stack[-1][1]
+            self.stack.pop()
+        
+        # Push current price and its span onto the stack
+        self.stack.append((price, span))
+        print(self.stack)
         return span
         
 
