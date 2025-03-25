a4578604ad2d16b2e8dc81cc1d5d320f5b6bfa58
PrabhatTheCoder <117260172+PrabhatTheCoder@users.noreply.github.com>
2025-03-25 11:02:49 +0530

    Time: 7 ms (51.77%), Space: 21.3 MB (64.55%) - LeetHub

---
+++ 0155-min-stack/0155-min-stack.py
@@ -0,0 +1,37 @@
+class MinStack:
+
+    # Time Complexity - O(1)
+    # Space Complexity - O(1)
+
+    def __init__(self):
+        self.min_ele = None
+        self.stack = []
+
+    def push(self, val: int) -> None:
+        if len(self.stack) == 0:
+            self.stack.append(val)
+            self.min_ele = val
+        else:
+            if val >= self.min_ele:
+                self.stack.append(val)
+            elif val <= self.min_ele:
+                self.stack.append(2 * val - self.min_ele)
+                self.min_ele = val
+
+    def pop(self) -> None:
+        if not self.stack:
+            return None
+        elif self.stack and self.stack[-1] <= self.min_ele:
+            self.min_ele = (2*self.min_ele) - self.stack[-1]
+            self.stack.pop()
+        else:
+            self.stack.pop()
+
+    def top(self) -> int:
+        if self.stack[-1] <= self.min_ele:
+            return self.min_ele
+        else:
+            return self.stack[-1]
+
+    def getMin(self) -> int:
+        return self.min_ele
