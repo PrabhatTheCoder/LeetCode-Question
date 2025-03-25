f127b5ac483d1cb507e31258c45c64af0ac61446
PrabhatTheCoder <117260172+PrabhatTheCoder@users.noreply.github.com>
2025-03-25 12:39:02 +0530

    Time: 88 ms (87.29%), Space: 49.1 MB (84.08%) - LeetHub

---
+++ 2573-remove-nodes-from-linked-list/2573-remove-nodes-from-linked-list.py
@@ -0,0 +1,36 @@
+# Definition for singly-linked list.
+# class ListNode:
+#     def __init__(self, val=0, next=None):
+#         self.val = val
+#         self.next = next
+class Solution:
+
+    def ReverseLinkedList(self,head):
+        prev = None
+        temp = head
+        while temp:
+            right = temp.next
+            temp.next = prev
+            prev = temp
+            temp = right
+        head = prev
+        return head
+
+    def removeNodes(self, head: Optional[ListNode]) -> Optional[ListNode]:
+        head = self.ReverseLinkedList(head)
+        dummy = ListNode(-1)
+        curr = dummy
+        temp = head
+        max_val = float('-inf')
+
+        while temp:
+            if temp.val >= max_val:
+                curr.next = temp
+                curr = curr.next
+                max_val = temp.val
+            temp = temp.next
+        
+        curr.next = None
+        head = self.ReverseLinkedList(dummy.next)
+        return head
+        
