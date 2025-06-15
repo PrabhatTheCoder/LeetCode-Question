fa916c47f98b93888cd319b00d2e8b04c9f244f7
PrabhatTheCoder <117260172+PrabhatTheCoder@users.noreply.github.com>
2025-06-16 00:18:14 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Basic/Sum of Binary Tree/sum-of-binary-tree.py
@@ -0,0 +1,14 @@
+'''
+class Node:
+    def __init__(self, val):
+        self.right = None
+        self.data = val
+        self.left = None
+'''        
+
+class Solution:
+    def sumBT(self, root):
+        if root == None:
+            return 0
+            
+        return root.data + self.sumBT(root.left) + self.sumBT(root.right)
