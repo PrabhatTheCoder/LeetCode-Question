895edd33bcf383105a9a15c36186a5efbaa28c9e
PrabhatTheCoder <117260172+PrabhatTheCoder@users.noreply.github.com>
2025-08-06 22:37:36 +0530

    Time: 84 ms (40.6%), Space: 37.8 MB (9.4%) - LeetHub

--- 1620-check-if-array-pairs-are-divisible-by-k/1620-check-if-array-pairs-are-divisible-by-k.py
+++ 1620-check-if-array-pairs-are-divisible-by-k/1620-check-if-array-pairs-are-divisible-by-k.py
@@ -5,15 +5,25 @@ class Solution:
         # Count frequencies of normalized remainders
         for num in arr:
             remainder = (num % k + k) % k
-            dic[remainder] = dic.get(remainder, 0) + 1
+            if remainder not in dic:
+                dic[remainder] = 1
+            else:
+                dic[remainder] += 1
 
         # Check remainder 0 separately (must be even)
-        if dic.get(0, 0) % 2 != 0:
+        if 0 in dic and dic[0] % 2 != 0:
             return False
+        elif 0 not in dic:
+            dic[0] = 0  # Ensure 0 exists for later checks
 
         # Check complementary remainders
         for i in range(1, k // 2 + 1):
-            if dic.get(i, 0) != dic.get(k - i, 0):
+            if i not in dic:
+                dic[i] = 0
+            if (k - i) not in dic:
+                dic[k - i] = 0
+
+            if dic[i] != dic[k - i]:
                 return False
 
         return True
