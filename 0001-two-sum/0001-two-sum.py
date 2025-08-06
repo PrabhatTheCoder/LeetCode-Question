789d065f23899b45112f8e5f5537024d048241a4
PrabhatTheCoder <117260172+PrabhatTheCoder@users.noreply.github.com>
2025-08-06 22:59:48 +0530

    Time: 3 ms (50.44%), Space: 19.6 MB (5.56%) - LeetHub

--- 0001-two-sum/0001-two-sum.py
+++ 0001-two-sum/0001-two-sum.py
@@ -1,14 +1,22 @@
-class Solution(object):
-    def twoSum(self, nums, target):
-        """
-        :type nums: List[int]
-        :type target: int
-        :rtype: List[int]
-        """
+class Solution:
+    def twoSum(self, nums: List[int], target: int) -> List[int]:
+        dic = {}
+        
+        # Build dictionary with value -> index
         for i in range(len(nums)):
-            val = target - nums[i]
-            for j in range(i+1,len(nums)):
-                if nums[j] == val:
-                  return [i,j]
-        return [-1,-1]
+            if nums[i] not in dic:
+                dic[nums[i]] = [i]  # store indices in a list
+            else:
+                dic[nums[i]].append(i)
+
+        # Check for pair
+        for key in dic:
+            find_ele = target - key
             
+            # Case 1: Pair with different numbers
+            if find_ele in dic and find_ele != key:
+                return [dic[key][0], dic[find_ele][0]]
+            
+            # Case 2: Pair with same number (need at least two occurrences)
+            if find_ele == key and len(dic[key]) > 1:
+                return [dic[key][0], dic[key][1]]
