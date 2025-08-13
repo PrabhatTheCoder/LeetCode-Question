b61f2b61e5232e35cb29ef099dc05bf1bb66ce04
PrabhatTheCoder <117260172+PrabhatTheCoder@users.noreply.github.com>
2025-08-13 23:18:46 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Medium/Kth Smallest/kth-smallest.py
@@ -0,0 +1,15 @@
+#User function Template for python3
+
+import heapq
+
+
+class Solution:
+
+    def kthSmallest(self, arr,k):
+        heapq.heapify(arr)
+
+        
+        for _ in range(k - 1):
+            heapq.heappop(arr)
+            
+        return heapq.heappop(arr)
