010d235106a8bfba42650ac9096b912d33bbd283
PrabhatTheCoder <117260172+PrabhatTheCoder@users.noreply.github.com>
2025-08-24 17:41:55 +0530

    Time: 4 ms (59.26%), Space: 25.8 MB (8.4%) - LeetHub

--- 0189-rotate-array/0189-rotate-array.py
+++ 0189-rotate-array/0189-rotate-array.py
@@ -1,39 +1,23 @@
-class Solution(object):
+class Solution:
+    def rotate(self, nums: List[int], k: int) -> None:
+        """
+        Do not return anything, modify nums in-place instead.
+        """
+        n = len(nums)
+        k = k % n 
 
-    def rotatePoint(self,nums,i,j):
-        
-        while i<=j:
-            temp = nums[i]
-            nums[i] = nums[j]
-            nums[j] = temp
-            i += 1
-            j -= 1
+        subArray = nums[len(nums)-k:]
+        move_idx = len(nums)-1-k
+
+        right = len(nums)-1
+        while move_idx >= 0:
+            nums[right] = nums[move_idx]
+            right -= 1
+            move_idx -= 1
+
+        for i in range(len(subArray)):
+            nums[i] = subArray[i]
 
         return nums
-
-    def rotate(self, nums, k):
-        """
-        :type nums: List[int]
-        :type k: int
-        :rtype: None Do not return anything, modify nums in-place instead.
-        """
-
-        # rotate left numsay
-        k = k % len(nums) # Handle cases where k is greater than the length of the array
-        j = len(nums) - k - 1
-        i = 0
-        nums = self.rotatePoint(nums,i,j)
-
-        # rotate right array
-        i = j + 1
-        j = len(nums) - 1
-        nums = self.rotatePoint(nums,i,j)
-
-        # rotate whole array
-        i = 0
-        j = len(nums) - 1
-        nums = self.rotatePoint(nums,i,j)
-        return nums
-        
 
         
