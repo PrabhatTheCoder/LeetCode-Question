28a1ee64e4cc97f57cbdd9e7bb5cbdec1ee01b7e
PrabhatTheCoder <117260172+PrabhatTheCoder@users.noreply.github.com>
2025-08-31 01:34:38 +0530

    Time: 0 ms (100%), Space: 17.8 MB (64.02%) - LeetHub

--- 0232-implement-queue-using-stacks/0232-implement-queue-using-stacks.py
+++ 0232-implement-queue-using-stacks/0232-implement-queue-using-stacks.py
@@ -1,32 +1,35 @@
 class MyQueue:
 
     def __init__(self):
-        self.input = []
-        self.output = []
-
+        self.stack1 = []
+        self.stack2 = []
+        
     def push(self, x: int) -> None:
-        self.input.append(x)
-
-    def pop(self) -> int:
-        if not self.output:
-            while self.input:
-                self.output.append(self.input.pop())
-        return self.output.pop()
-
-    def peek(self) -> int:
-        if not self.output:
-            while self.input:
-                self.output.append(self.input.pop())
-        return self.output[-1]
-
-    def empty(self) -> bool:
-        return not self.input and not self.output
+        self.stack1.append(x)
         
 
+    def pop(self) -> int:
+        while self.stack1:
+            self.stack2.append(self.stack1.pop())
+        
+        val = self.stack2.pop()
+        
+        while self.stack2:
+            self.stack1.append(self.stack2.pop())
+        
+        return val
+        
 
-# Your MyQueue object will be instantiated and called as such:
-# obj = MyQueue()
-# obj.push(x)
-# param_2 = obj.pop()
-# param_3 = obj.peek()
-# param_4 = obj.empty()
+    def peek(self) -> int:
+        while self.stack1:
+            self.stack2.append(self.stack1.pop())
+        
+        val = self.stack2[-1]
+        
+        while self.stack2:
+            self.stack1.append(self.stack2.pop())
+        
+        return val
+        
+    def empty(self) -> bool:
+        return len(self.stack1) == 0
