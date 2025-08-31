013c21fa7ea1af7ba5284fafffca0b76ef300441
PrabhatTheCoder <117260172+PrabhatTheCoder@users.noreply.github.com>
2025-08-31 14:11:02 +0530

    Time: 8 ms (49.57%), Space: 18.5 MB (54.18%) - LeetHub

--- 0860-design-circular-queue/0860-design-circular-queue.py
+++ 0860-design-circular-queue/0860-design-circular-queue.py
@@ -1,53 +1,49 @@
 class MyCircularQueue:
 
     def __init__(self, k: int):
-        self.curr_size = 0
-        self.capacity = k
-        self.back = 0
+        self.queue = [None]*k
         self.front = 0
-        self.dequeue = [None] * k
+        self.rear = 0
+        self.k = k
 
     def enQueue(self, value: int) -> bool:
-        if self.curr_size == self.capacity:
+        if self.queue[self.rear]:
             return False
-        self.dequeue[self.back] = value
-        self.back = (self.back + 1) % self.capacity
-        self.curr_size += 1
+        self.queue[self.rear] = value
+        self.rear = (self.rear + 1) % self.k
         return True
 
     def deQueue(self) -> bool:
-        if self.curr_size == 0:
+        if self.queue[self.front] == None:
             return False
-        self.front = (self.front + 1) % self.capacity
-        self.curr_size -= 1
+        self.queue[self.front] = None
+        self.front = (self.front + 1) % self.k
         return True
 
     def Front(self) -> int:
-        if self.curr_size == 0:
+        if self.isEmpty():
             return -1
-        return self.dequeue[self.front]
-
+        return self.queue[self.front]
+        
     def Rear(self) -> int:
-        if self.curr_size == 0:
+        if self.isEmpty():
             return -1
-        return self.dequeue[(self.back - 1 + self.capacity) % self.capacity]
+        return self.queue[(self.rear - 1 + self.k) % self.k]
 
     def isEmpty(self) -> bool:
-        return self.curr_size == 0
+        return self.front == self.rear and self.queue[self.front] is None
+        
 
     def isFull(self) -> bool:
-        return self.curr_size == self.capacity
+        return self.front == self.rear and self.queue[self.front] is not None
+        
 
-# Test Case
-# obj = MyCircularQueue(3)
-# print(obj.enQueue(2))    # True
-# print(obj.Rear())        # 2
-# print(obj.Front())       # 2
-# print(obj.deQueue())     # True
-# print(obj.Front())       # -1
-# print(obj.deQueue())     # False
-# print(obj.Front())       # -1
-# print(obj.enQueue(4))    # True
-# print(obj.enQueue(2))    # True
-# print(obj.enQueue(2))    # True
-# print(obj.enQueue(3))    # False
+
+# Your MyCircularQueue object will be instantiated and called as such:
+# obj = MyCircularQueue(k)
+# param_1 = obj.enQueue(value)
+# param_2 = obj.deQueue()
+# param_3 = obj.Front()
+# param_4 = obj.Rear()
+# param_5 = obj.isEmpty()
+# param_6 = obj.isFull()
