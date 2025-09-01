5ec41cb32ade3d68b4d282c1802836b48d97d9de
PrabhatTheCoder <117260172+PrabhatTheCoder@users.noreply.github.com>
2025-09-02 01:27:55 +0530

    Time: 29 ms (95.7%), Space: 17.8 MB (23.24%) - LeetHub

--- 1013-fibonacci-number/1013-fibonacci-number.py
+++ 1013-fibonacci-number/1013-fibonacci-number.py
@@ -1,16 +1,16 @@
 class Solution:
-
-    def solve(self, n, dp):
-        if n <= 1:
-            return n
-        if dp[n] != -1:
-            return dp[n]
-        dp[n] = self.solve(n-1, dp) + self.solve(n-2, dp)
-        return dp[n]
         
     def fib(self, n: int) -> int:
-        # Recursion + Memoisation
+        # Tabulation
+        
+        if n == 0:
+            return n
         dp = [-1] * (n+1)
-        return self.solve(n, dp)
+        dp[0] = 0
+        dp[1] = 1
+        for i in range(2, n+1):
+            dp[i] = dp[i-1] + dp[i-2]
+        print(dp)
+        return dp[n]
 
         
