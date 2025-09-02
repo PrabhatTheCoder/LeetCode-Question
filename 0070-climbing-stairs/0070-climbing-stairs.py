a626a5ce632ffba025977ccccbc9a2d16dd3b558
PrabhatTheCoder <117260172+PrabhatTheCoder@users.noreply.github.com>
2025-09-02 16:30:54 +0530

    Time: 0 ms (100%), Space: 17.6 MB (78.5%) - LeetHub

--- 0070-climbing-stairs/0070-climbing-stairs.py
+++ 0070-climbing-stairs/0070-climbing-stairs.py
@@ -1,11 +1,12 @@
 class Solution:
-    def climbStairs(self, n: int,  memo={}) -> int:
-        if n in memo:  # Check if result is already computed
-            return memo[n]
+    def climbStairs(self, n: int) -> int:
         if n == 1:
             return 1
-        if n == 2:
-            return 2
-        memo[n] = self.climbStairs(n - 1, memo) + self.climbStairs(n - 2, memo)
-        return memo[n]
-        
+        dp = [0] * (n + 1)  
+        dp[1] = 1
+        dp[2] = 2
+
+        for i in range(3, n + 1):
+            dp[i] = dp[i - 1] + dp[i - 2]
+
+        return dp[n]
