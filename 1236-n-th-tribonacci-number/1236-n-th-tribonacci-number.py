f038ff167b6e9c4e6cd0f746b8db5d9e0255da42
PrabhatTheCoder <117260172+PrabhatTheCoder@users.noreply.github.com>
2025-09-02 18:06:23 +0530

    Time: 0 ms (100%), Space: 18 MB (8.18%) - LeetHub

--- 1236-n-th-tribonacci-number/1236-n-th-tribonacci-number.py
+++ 1236-n-th-tribonacci-number/1236-n-th-tribonacci-number.py
@@ -1,22 +1,20 @@
-class Solution:
-    def solve(self, n, dp):
-        if n == 0:
-            dp[0] = 0
-            return dp[0]
-        if n == 1:
-            dp[1] = 1
-            return dp[1]
-        if n == 2:
-            dp[2] = 1
-            return dp[2]
-        if dp[n] != 0:
-            return dp[n]
-        
-        dp[n] = self.solve(n-1, dp) + self.solve(n-2, dp) + self.solve(n-3, dp)
-        return dp[n]
-        
+class Solution:     
 
     def tribonacci(self, n: int) -> int:
-        dp = [0] * (n + 1)
-        return self.solve(n, dp)
+        dp = [0] * (n+1)
+        if n == 0:
+            return 0
+        if n == 1:
+            return 1
+        if n == 2:
+            return 1
+            
+        dp[0] = 0
+        dp[1] = 1
+        dp[2] = 1
+
+        for i in range(3,n+1):
+            dp[i] = dp[i-1] + dp[i-2] + dp[i-3]
+
+        return dp[n]
         
