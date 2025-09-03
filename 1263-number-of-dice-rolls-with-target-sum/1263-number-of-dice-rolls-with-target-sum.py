0931cb9d2d8406f56a5ace2b8383e7fe21937b13
PrabhatTheCoder <117260172+PrabhatTheCoder@users.noreply.github.com>
2025-09-03 13:10:00 +0530

    Time: 443 ms (18.24%), Space: 18.2 MB (78.62%) - LeetHub

--- 1263-number-of-dice-rolls-with-target-sum/1263-number-of-dice-rolls-with-target-sum.py
+++ 1263-number-of-dice-rolls-with-target-sum/1263-number-of-dice-rolls-with-target-sum.py
@@ -1,23 +1,16 @@
+MOD = 10**9 + 7
+
 class Solution:
-    MOD = 10**9 + 7
-
-    def solve(self, n, k, target):
-        if target < 0:
-            return 0
-        if n == 0:
-            return 1 if target == 0 else 0
-
-        if self.t[n][target] != -1:
-            return self.t[n][target]
-
-        ways = 0
-        for face in range(1, k+1):
-            ways = (ways + self.solve(n-1, k, target-face)) % self.MOD
-
-        self.t[n][target] = ways
-        return self.t[n][target]
-
     def numRollsToTarget(self, n: int, k: int, target: int) -> int:
-        # dp[n][target]
-        self.t = [[-1]*(target+1) for _ in range(n+1)]
-        return self.solve(n, k, target)
+        # dp[i][j] = number of ways to get sum j with i dice
+        dp = [[0] * (target + 1) for _ in range(n + 1)]
+        dp[0][0] = 1  # 1 way to get sum 0 with 0 dice
+
+        for i in range(1, n + 1):  # number of dice
+            for j in range(1, target + 1):  # target sum
+            
+                for face in range(1, k + 1):  # dice face values
+                    if j >= face:
+                        dp[i][j] = (dp[i][j] + dp[i - 1][j - face]) % MOD
+
+        return dp[n][target]
