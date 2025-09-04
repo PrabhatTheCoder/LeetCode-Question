7a1100c8faac1caf2e78a1a95dba60e7f2021d31
PrabhatTheCoder <117260172+PrabhatTheCoder@users.noreply.github.com>
2025-09-04 12:59:56 +0530

    Time: 0 ms (100%), Space: 17.8 MB (34.63%) - LeetHub

--- 0019-remove-nth-node-from-end-of-list/0019-remove-nth-node-from-end-of-list.py
+++ 0019-remove-nth-node-from-end-of-list/0019-remove-nth-node-from-end-of-list.py
@@ -1,19 +1,44 @@
+# Definition for singly-linked list.
+# class ListNode:
+#     def __init__(self, val=0, next=None):
+#         self.val = val
+#         self.next = next
 class Solution:
+
+    def removeNode(self, head, n):
+        idx = 0
+        dummy = ListNode(-1)
+        dummy.next = head
+
+        temp = dummy
+        while temp:
+            
+            if idx == n:
+                prev.next = temp.next
+            idx += 1
+            prev = temp
+            temp = temp.next
+            
+        return dummy.next
+
+    def reverseList(self, head):
+        prev = None
+        curr = head
+
+        while curr:
+            temp = curr.next     # save next node
+            curr.next = prev     # reverse link
+            prev = curr          # move prev
+            curr = temp          # move curr
+
+        return prev   # new head
+
+        
     def removeNthFromEnd(self, head: Optional[ListNode], n: int) -> Optional[ListNode]:
-       
-        slow = head
-        fast = head
+        head = self.reverseList(head)
+        temp = self.removeNode(head, n)
+        return self.reverseList(temp)
 
-        for i in range(n+1):
-            if fast == None:
-                return head.next
-            fast = fast.next
-        
-        while fast != None:
-            slow = slow.next
-            fast = fast.next
 
-        slow.next = slow.next.next
-        return head
+
         
-    
