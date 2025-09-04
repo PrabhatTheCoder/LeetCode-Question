ee5b357c4c7cf296d97e40742819098899e03484
PrabhatTheCoder <117260172+PrabhatTheCoder@users.noreply.github.com>
2025-09-05 03:33:16 +0530

    Time: 876 ms (5.11%), Space: 15.4 MB (5.73%) - LeetHub

--- 0102-binary-tree-level-order-traversal/0102-binary-tree-level-order-traversal.py
+++ 0102-binary-tree-level-order-traversal/0102-binary-tree-level-order-traversal.py
@@ -6,31 +6,35 @@
 #         self.right = right
 
 class Solution(object):
+
+    def level(self, root):
+        if not root:
+            return 0
+        left = self.level(root.left)
+        right = self.level(root.right)
+        return 1 + max(left, right)
+
+    def solve(self, root, i, target):
+        if not root:
+            return []
+        if i == target:
+            return [root.val]
+
+        left = self.solve(root.left, i+1, target)
+        right = self.solve(root.right, i+1, target)
+
+        return left + right
+
     def levelOrder(self, root):
         """
         :type root: Optional[TreeNode]
         :rtype: List[List[int]]
         """
-        if not root:
-            return []
+        ans = []
+        height = self.level(root)
 
-        queue = [root]
-        result = []
-        while queue:
-            level_size = len(queue)
-            ans = []
+        for i in range(height):
+            val = self.solve(root, 0, i)
+            ans.append(val)
 
-            for _ in range(level_size):
-                node = queue.pop(0)  # pop from front (acts as a queue)
-                ans.append(node.val)
-
-                if node.left:
-                    queue.append(node.left)
-                if node.right:
-                    queue.append(node.right)
-
-            result.append(ans)
-
-        return result
-
-                
+        return ans
