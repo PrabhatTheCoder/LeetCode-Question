261bca2ea6010ed5cb4dbfb329bb1ed22a35a67e
PrabhatTheCoder <117260172+PrabhatTheCoder@users.noreply.github.com>
2025-09-13 11:29:13 +0530

    Time: 201 ms (73.59%), Space: 18.1 MB (78.94%) - LeetHub

--- 1263-number-of-dice-rolls-with-target-sum/1263-number-of-dice-rolls-with-target-sum.py
+++ 1263-number-of-dice-rolls-with-target-sum/1263-number-of-dice-rolls-with-target-sum.py
@@ -1,16 +1,27 @@
 MOD = 10**9 + 7
 
 class Solution:
+
+    # Recursion  + Memoisation
+    
+    def solve(self, n, k, target):
+
+        if n == 0 and target == 0:
+            return 1
+        if n == 0 or target < 0:
+            return 0
+        
+        if self.dp[n][target] != -1:
+            return self.dp[n][target]
+        
+        ways = 0
+        for i in range(1,k+1):
+            ways += self.solve(n-1, k, target - i) 
+
+        self.dp[n][target] = ways % MOD
+        return self.dp[n][target]
+
     def numRollsToTarget(self, n: int, k: int, target: int) -> int:
-        # dp[i][j] = number of ways to get sum j with i dice
-        dp = [[0] * (target + 1) for _ in range(n + 1)]
-        dp[0][0] = 1  # 1 way to get sum 0 with 0 dice
-
-        for i in range(1, n + 1):  # number of dice
-            for j in range(1, target + 1):  # target sum
-            
-                for face in range(1, k + 1):  # dice face values
-                    if j >= face:
-                        dp[i][j] = (dp[i][j] + dp[i - 1][j - face]) % MOD
-
-        return dp[n][target]
+        self.dp = [[-1] * (target+1) for i in range(n+1)]
+        return self.solve(n,k,target)
+        
